22b336423aa6072108a704a393ac1de672145920
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2026-01-28 19:37:50 +0530

    new repo

---
+++ src/main/java/AutomationClasses/AboutPage.java
@@ -0,0 +1,48 @@
+package AutomationClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AboutPage {
+
+	@Test
+	public  void validateHomePage() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.yessinfotech.com/");
+		Thread.sleep(6000);
+		driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
+		driver.findElement(By.linkText("About Us")).click();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+
+		System.out.println("Hello World");
+
+		System.out.println("hi");
+
+		if(title.equals("About Us | yes")) {
+			System.out.println("page validation done");
+		}else
+		{
+			System.out.println("page not landing to the about us page");
+		}
+		
+		driver.findElement(By.linkText("Home")).click();
+		
+		
+		String homeTitle = driver.getTitle();
+		
+		if(homeTitle.equals("Software training classes institute in Hadapsar Pune")) {
+		System.out.println("successfully validated homepage");	
+		}else {
+			System.out.println("please check homePage it is not matching with expectation");
+		}
+		
+		driver.close();
+	}
+	
+}
